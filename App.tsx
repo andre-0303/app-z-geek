b3535df0af59b2ebeaf3d56b78639683b2237eec
andre-0303 <bandeiradev.ti@gmail.com>
2025-06-19 18:31:28 -0300

    Melhorando a UI

--- App.tsx
+++ App.tsx
@@ -1,23 +1,36 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import HomeScreen from "./src/screens/HomeScreen";
-import GamesScreen from "./src/screens/GamesScreen";
+import { useFonts } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import GamesScreen from './src/screens/GamesScreen';
+import { Text, View } from 'react-native';
 
 export type RootStackParamList = {
   Home: undefined;
   Games: undefined;
-}
+};
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
+        <Text>Carregando fontes...</Text>
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Bem-vindo ao Z-Games 🎮'}} />
-        <Stack.Screen name="Games" component={GamesScreen} options={{title: 'Jogos'}} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Games" component={GamesScreen} options={{ title: 'Jogos' }} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,65 @@
-export default function HomeScreen() {
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from "../../App";
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+export default function HomeScreen({ navigation }: Props) {
     return (
-        <>
-            
-        </>
-    )
+        <View style={styles.container}>
+            <Text style={styles.title}>Z-Geek</Text>
+            <Text style={styles.subtitle}>App de Indicação de jogos</Text>
+
+            <TouchableOpacity 
+                style={styles.button} 
+                onPress={() => navigation.navigate('Games')}
+                activeOpacity={0.8}
+            >
+                <Text style={styles.buttonText}>Ver Jogos</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#67159C',
+        padding: 20,
+    },
+    title: {
+        color: '#fff',
+        fontSize: 36,
+        marginBottom: 5,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Bold', 
+    },
+    subtitle: {
+        color: '#bbb',
+        marginBottom: 30,
+        fontSize: 17,
+    },
+    button: {
+        backgroundColor: '#1D4ED8',
+        paddingVertical: 10,
+        paddingHorizontal: 32,
+        borderRadius: 980, 
+        elevation: 5, 
+        shadowColor: '#000', 
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        marginBottom: 20,
+        marginVertical: -10
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '400',
+        textAlign: 'center',
+        fontFamily: 'Poppins-Bold',
+    }
+});
